b80c29ea0a109c3a87aebd79c41b54d38288d08a
Bwhitman33 <bwhitman33@gmail.com>
2023-05-21 16:30:23 -0400

    wrapped datestamo into doc ready function and added function to parse through timeblocks in relation to current, past and future hour

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,29 +1,43 @@
-
-var todayDate = dayjs().format('dddd, MMMM D, YYYY');
-$('#currentDay').html(todayDate);
-
 $(document).ready(function () {
-    $('.saveBtn').on('click', function () {
-      var text = $(this).siblings('.description').val();
-      var time = $(this).parent().attr('id');
+
+    var todayDate = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+    $('#currentDay').html(todayDate);
+
+    $('.saveBtn').on('click', function() {
+      console.log(this);
+      var text = $(this).siblings("description").val();
+      var time = $(this).parent().attr("id");
       localStorage.setItem(time, text);
     })
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+function timeSchedule() {
+    var timeNow = dayjs().get('hour');
+   
+    $(".time-block").each(function () {
+        var timeBlock = parseInt($(this).attr("id").split("hour-")[1]);
+        console.log(timeBlock, timeNow)
+
+        if (timeBlock < timeNow) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (timeBlock === timeNow) {
+            $(this).addClass("present");
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).addClass("future");
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+        }
+    })
+}
+
+    timeSchedule();
+
+    
+
+
 });
